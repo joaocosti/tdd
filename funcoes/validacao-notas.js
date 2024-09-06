d5bc83bec6eed2eaa2cce06a1cd5863c44881c44
joaocosti <joaomartins@vipcommerce.com.br>
2024-09-06 20:03:57 -0300

    Refatorando o método determinarAprovacao para passar nos casos de testes base

--- funcoes/validacao-notas.js
+++ funcoes/validacao-notas.js
@@ -1,5 +1,12 @@
 class Aluno {
     determinarAprovacao(frequencia, notaFinal, NotaEspecial) {
+        const MENSAGEM_REPROVADO = 'Reprovado';
+        const MENSAGEM_APROVADO = 'Aprovado';
+
+        if (frequencia < 75)
+            return MENSAGEM_REPROVADO;
+        else if (notaFinal >= 60)
+            return MENSAGEM_APROVADO;
         return 0
     }
 }
