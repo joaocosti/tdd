a31d61d60b0f262110b8a949742e2be5cc9d226a
joaocosti <joaomartins@vipcommerce.com.br>
2024-09-06 20:08:32 -0300

    Refatorando o método para suporte aos casos de exceção

--- funcoes/validacao-notas.js
+++ funcoes/validacao-notas.js
@@ -3,15 +3,16 @@ class Aluno {
         const MENSAGEM_REPROVADO = 'Reprovado';
         const MENSAGEM_APROVADO = 'Aprovado';
 
-        if (frequencia < 75)
+        if (frequencia < 0 || notaFinal < 0 || notaEspecial < 0)
+            return MENSAGEM_REPROVADO;
+        else if (frequencia < 75)
             return MENSAGEM_REPROVADO;
         else if (notaFinal >= 60)
             return MENSAGEM_APROVADO;
-        else if(((notaFinal+ notaEspecial)/2) < 60)
+        else if (((notaFinal + notaEspecial) / 2) < 60)
             return MENSAGEM_REPROVADO
-        else if(((notaFinal+ notaEspecial)/2) >= 60)
+        else if (((notaFinal + notaEspecial) / 2) >= 60)
             return MENSAGEM_APROVADO
-        return 0
     }
 }
 
