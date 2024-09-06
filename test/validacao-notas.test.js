180281844bce154b3633c9cdc1d77a856912ab1f
joaocosti <joaomartins@vipcommerce.com.br>
2024-09-06 20:02:32 -0300

    Teste unitário base do caso

--- test/validacao-notas.test.js
+++ test/validacao-notas.test.js
@@ -1,2 +1,19 @@
 const Aluno = require('../funcoes/validacao-notas');
 
+let ALUNO
+beforeAll(() => {
+    ALUNO = new Aluno();
+});
+
+describe('Teste do metodo determinarAprovacao - Casos Base', () => {
+
+    test('Frequencia menor que 75', () => {
+        expect(ALUNO.determinarAprovacao(74, 100, 100)).toEqual('Reprovado');
+    });
+
+    test('Nota Final igual a 60', () => {
+        expect(ALUNO.determinarAprovacao(100, 60, 100)).toEqual('Aprovado');
+    });
+});
+
+
