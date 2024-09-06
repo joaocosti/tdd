b3c462f580da8435d45e27ba24041ddc5a9fe2b5
joaocosti <joaomartins@vipcommerce.com.br>
2024-09-06 20:06:33 -0300

    Refatorando o método para passar nos demais casos de teste

--- funcoes/validacao-notas.js
+++ funcoes/validacao-notas.js
@@ -1,5 +1,5 @@
 class Aluno {
-    determinarAprovacao(frequencia, notaFinal, NotaEspecial) {
+    determinarAprovacao(frequencia, notaFinal, notaEspecial) {
         const MENSAGEM_REPROVADO = 'Reprovado';
         const MENSAGEM_APROVADO = 'Aprovado';
 
@@ -7,6 +7,10 @@ class Aluno {
             return MENSAGEM_REPROVADO;
         else if (notaFinal >= 60)
             return MENSAGEM_APROVADO;
+        else if(((notaFinal+ notaEspecial)/2) < 60)
+            return MENSAGEM_REPROVADO
+        else if(((notaFinal+ notaEspecial)/2) >= 60)
+            return MENSAGEM_APROVADO
         return 0
     }
 }
