cc4dd14a6010612a18ca883520427092f6e45b4c
joaocosti <joaomartins@vipcommerce.com.br>
2024-09-06 20:09:28 -0300

    Refatorando o código para deixá-lo mais legível e organizado

--- funcoes/validacao-notas.js
+++ funcoes/validacao-notas.js
@@ -3,16 +3,16 @@ class Aluno {
         const MENSAGEM_REPROVADO = 'Reprovado';
         const MENSAGEM_APROVADO = 'Aprovado';
 
-        if (frequencia < 0 || notaFinal < 0 || notaEspecial < 0)
+        if (frequencia < 0 || notaFinal < 0 || notaEspecial < 0) {
             return MENSAGEM_REPROVADO;
-        else if (frequencia < 75)
+        } else if (frequencia < 75) {
             return MENSAGEM_REPROVADO;
-        else if (notaFinal >= 60)
+        } else if (notaFinal >= 60) {
             return MENSAGEM_APROVADO;
-        else if (((notaFinal + notaEspecial) / 2) < 60)
-            return MENSAGEM_REPROVADO
-        else if (((notaFinal + notaEspecial) / 2) >= 60)
-            return MENSAGEM_APROVADO
+        }
+
+        const MEDIA = (notaFinal + notaEspecial) / 2;
+        return MEDIA >= 60 ? MENSAGEM_APROVADO : MENSAGEM_REPROVADO;
     }
 }
 
